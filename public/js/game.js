0866efcf0f6e91ecf77c45c04adfc22f8fe17a92
Carl Reiner <creiner1@gmail.com>
2015-10-13 10:06:08 -0700

    broke Player into its own js

--- public/js/game.js
+++ public/js/game.js
@@ -119,39 +119,6 @@ TilePool.prototype.giveTile = function() {
   return this.tiles.pop();
 }
 
-//Player constructor
-var Player = function(name) {
-  this.name = name;
-  this.rack = [];
-  this.score = 0;
-}
-
-//Draw a number of tiles from the specified pool
-//If no number is specified, it will draw until the rack is full (7 tiles)
-Player.prototype.draw = function(pool, numTiles) {
-  if(numTiles) {
-    for(var i = 0; i < numTiles; i++) {
-      this.rack.push(pool.giveTile());
-    }
-  } else {
-    while(this.rack.length < 7) {
-      this.rack.push(pool.giveTile());
-    }
-  }
-  this.renderRack();
-}
-
-//Add a number to the player's score
-Player.prototype.incrementScore = function(num) {
-  this.score += num;
-}
-
-//Remove a tile from the player's rack
-Player.prototype.removeTileFromRack = function(num) {
-  this.rack.splice(this.rack.indexOf(num), 1);
-  this.renderRack();
-}
-
 //Render the game board in the DOM
 Game.prototype.renderBoard = function() {
   var domBoard = $('#board');
@@ -165,21 +132,6 @@ Game.prototype.renderBoard = function() {
       }
     }
   }
-}
-
-//Render a player's rack in the DOM
-Player.prototype.renderRack = function() {
-  var domRack = $('#rack');
-  domRack.children().remove();
-  for(var i = 0; i < this.rack.length; i++) {
-    domRack.append('<div class="square rack-' + i + '">' + this.rack[i] + '</div>');
-  }
-}
-
-//Render the player's info to the DOM
-Player.prototype.renderScore = function() {
-  $('#active-player').text(this.name);
-  $('#score').text(this.score);
 }
 
 var selectedSquare = null;
---
+++ public/js/player.js
@@ -0,0 +1,47 @@
+//Player constructor
+var Player = function(name) {
+  this.name = name;
+  this.rack = [];
+  this.score = 0;
+}
+
+//Draw a number of tiles from the specified pool
+//If no number is specified, it will draw until the rack is full (7 tiles)
+Player.prototype.draw = function(pool, numTiles) {
+  if(numTiles) {
+    for(var i = 0; i < numTiles; i++) {
+      this.rack.push(pool.giveTile());
+    }
+  } else {
+    while(this.rack.length < 7) {
+      this.rack.push(pool.giveTile());
+    }
+  }
+  this.renderRack();
+}
+
+//Add a number to the player's score
+Player.prototype.incrementScore = function(num) {
+  this.score += num;
+}
+
+//Remove a tile from the player's rack
+Player.prototype.removeTileFromRack = function(num) {
+  this.rack.splice(this.rack.indexOf(num), 1);
+  this.renderRack();
+}
+
+//Render a player's rack in the DOM
+Player.prototype.renderRack = function() {
+  var domRack = $('#rack');
+  domRack.children().remove();
+  for(var i = 0; i < this.rack.length; i++) {
+    domRack.append('<div class="square rack-' + i + '">' + this.rack[i] + '</div>');
+  }
+}
+
+//Render the player's info to the DOM
+Player.prototype.renderScore = function() {
+  $('#active-player').text(this.name);
+  $('#score').text(this.score);
+}
