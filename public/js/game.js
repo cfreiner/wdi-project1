fde6fe689e68b8cfd82744c536f971f594b902d7
Carl Reiner <creiner1@gmail.com>
2015-10-12 17:32:05 -0700

    modified event listeners for selection

--- public/js/game.js
+++ public/js/game.js
@@ -190,9 +190,16 @@ var selectSquare = function(e) {
   }
 }
 
-var chooseTile = function(e) {
-  if($('.chosen').length === 0 {
-
+//Returns a function for use in the click listener
+//Param should be "#board" or "#rack"
+var selection = function(parentElement) {
+  return function(e) {
+    e.stopPropagation();
+    var selected = $(parentElement + ' .selected');
+    if(selected.length > 0) {
+      selected.toggleClass('selected');
+    }
+    $(e.target).toggleClass('selected');
   }
 }
 
@@ -422,7 +429,9 @@ Game.prototype.determineEndOfTurn = function(player) {
   }
 }
 
-$('#rack, #board').on('click', 'div', selectSquare);
+// $('#rack, #board').on('click', 'div', selectSquare);
+$('#board').on('click', 'div', selection('#board'));
+$('#rack').on('click', 'div', selection('#rack'));
 
 $('#move-btn').on('click', function() {
   game.commitMove();
